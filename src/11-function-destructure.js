1ac4bc5a95ea5794d11becd97aa2cd2b9464c337
Shayan Mondal <shayonmondal@gmail.com>
2021-06-06 16:14:22 +0530

    js

--- src/11-function-destructure.js
+++ src/11-function-destructure.js
@@ -1,17 +1,14 @@
-function createEmployee ({username,email,mobile,password}) //this{} adds flexibility to user , input can be added in any order.
-{ 
-
-    console.log(`Username ${username}`)
-    console.log(`Username ${email}`)
-    console.log(`Username ${mobile}}`)
-    console.log(`Username ${password}`)
-
+function createEmployee({ username, email, mobile, password }) {
+  //this{} adds flexibility to user , input can be added in any order.
+  console.log(`Username ${username}`);
+  console.log(`Username ${email}`);
+  console.log(`Username ${mobile}}`);
+  console.log(`Username ${password}`);
 }
 //calling the function with object inside it.
 createEmployee({
-    username : "Shayan",
-    email : "shay@gmail",
-    mobile : "56712",
-    password : "45dsc"
+  username: "Shayan",
+  email: "shay@gmail",
+  mobile: "56712",
+  password: "45dsc",
 });
-
